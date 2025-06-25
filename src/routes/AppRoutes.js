a7b2ff1e1933833696237f36f90da39df3ac594f
Harsh Kolte <74823522+harshkolte01@users.noreply.github.com>
2025-06-25 12:48:29 +0530

    routing added

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,20 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
-import Login from '../pages/Login';
+import Login from '../pages/Login'
 import Products from '../pages/Products';
 import Cart from '../pages/Cart';
 import About from '../pages/About';
 import Contact from '../pages/Contact';
 import ProtectedRoute from '../components/ProtectedRoute';
+import Navbar from '../components/Navbar';
 import { useState } from 'react';
 
 export default function AppRoutes() {
   const [username, setUsername] = useState('');
   return (
     <Router>
+      <Navbar username={username} />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="login" element={<Login onLogin={(name) => setUsername(name)} />} />
+        <Route path="Login" element={<Login onLogin={(name) => setUsername(name)} />} />
         <Route path="/products" element={<ProtectedRoute><Products /></ProtectedRoute>} />
         <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
         <Route path="/about" element={<ProtectedRoute><About /></ProtectedRoute>} />
